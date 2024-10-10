a5c4c8d58f8ec6e145bf1b96702b3563efadf23d
cambios hechos en la universidad <>
2024-10-10 17:32:01 -0500

    Avance 1 del código

--- src/Datos.py
+++ src/Datos.py
@@ -1,7 +1,26 @@
 
 def main():
-    #Tu código va aquí. Mantén la indentación
-    pass # borra esta línea cuando con inicies tu código
+    def Congfiguracion_de_admministrador():
+        
+        pass
+    control = True
+    while control == True:
+        lista1 = [1, 2]
+        print("1. Cliente\n2.Administrador")
+        a = int(input(f"Por favor seleccione su rol: "))
+        if a not in lista1:
+            print("Por favor seleccione una opción correcta")
+        elif a == 1:
+            print()
+            control = False
+        elif a == 2:
+            cont = str(input("Por favor ingrese su contraseña: "))
+            if cont == contraseña:
+                
+            control = False
+
+
+         
 
 
 if __name__ == "__main__":
